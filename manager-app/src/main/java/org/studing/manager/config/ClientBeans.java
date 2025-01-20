3c87bf24f769b0f0bc35e53749f4bac6f8bf3db2
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-20 18:18:20 +0300

    Basic authentication

--- manager-app/src/main/java/org/studing/manager/config/ClientBeans.java
+++ manager-app/src/main/java/org/studing/manager/config/ClientBeans.java
@@ -3,6 +3,7 @@ package org.studing.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.studing.manager.client.ProductsRestClientImpl;
 
 import static org.springframework.web.client.RestClient.builder;
@@ -11,7 +12,15 @@ import static org.springframework.web.client.RestClient.builder;
 public class ClientBeans {
 
     @Bean
-    public ProductsRestClientImpl productsRestClient(@Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
-        return new ProductsRestClientImpl(builder().baseUrl(catalogueBaseUri).build());
+    public ProductsRestClientImpl productsRestClient(
+        @Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+        @Value("${shop.services.catalogue.username:}") String catalogueUsername,
+        @Value("${shop.services.catalogue.password:}") String cataloguePassword) {
+
+        return new ProductsRestClientImpl(
+            builder()
+                .baseUrl(catalogueBaseUri)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
+                .build());
     }
 }
