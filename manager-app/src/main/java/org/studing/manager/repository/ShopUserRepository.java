93cbcae54927c760220fe36cc0b4d25c15899a3b
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-20 19:19:35 +0300

    Added repository

---
+++ manager-app/src/main/java/org/studing/manager/repository/ShopUserRepository.java
@@ -0,0 +1,11 @@
+package org.studing.manager.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.studing.manager.entity.ShopUser;
+
+import java.util.Optional;
+
+
+public interface ShopUserRepository extends CrudRepository<ShopUser, Integer> {
+    Optional<ShopUser> findByUsername(String username);
+}
