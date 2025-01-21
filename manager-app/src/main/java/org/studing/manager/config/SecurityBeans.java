72c62d8512dc2ffdbefe8d94b4650ff258bcbd8a
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-21 16:46:14 +0300

    Added method oAuth2UserService

--- manager-app/src/main/java/org/studing/manager/config/SecurityBeans.java
+++ manager-app/src/main/java/org/studing/manager/config/SecurityBeans.java
@@ -1,10 +1,22 @@
 package org.studing.manager.config;
 
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Stream.concat;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -14,8 +26,27 @@ public class SecurityBeans {
         return http
             .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                 .anyRequest()
-                .authenticated())
+                .hasRole("MANAGER")
+
+            )
             .oauth2Login(withDefaults())
             .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+        val oidcUserService = new OidcUserService();
+        return userRequest -> {
+            val oidcUser = oidcUserService.loadUser(userRequest);
+            List<GrantedAuthority> authorities = concat(oidcUser.getAuthorities().stream(),
+                ofNullable(oidcUser.getClaimAsStringList("groups"))
+                .orElseGet(List::of)
+                .stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .map(GrantedAuthority.class::cast))
+                .toList();
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
     }
 }
