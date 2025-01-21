dfd96de5a1c05914d50373bb0eef4c0b56770ca0
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-21 11:24:20 +0300

    revert

--- manager-app/src/main/java/org/studing/manager/config/ClientBeans.java
+++ manager-app/src/main/java/org/studing/manager/config/ClientBeans.java
@@ -3,7 +3,6 @@ package org.studing.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.studing.manager.client.ProductsRestClientImpl;
 
 import static org.springframework.web.client.RestClient.builder;
@@ -12,15 +11,7 @@ import static org.springframework.web.client.RestClient.builder;
 public class ClientBeans {
 
     @Bean
-    public ProductsRestClientImpl productsRestClient(
-        @Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
-        @Value("${shop.services.catalogue.username:}") String catalogueUsername,
-        @Value("${shop.services.catalogue.password:}") String cataloguePassword) {
-
-        return new ProductsRestClientImpl(
-            builder()
-                .baseUrl(catalogueBaseUri)
-                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
-                .build());
+    public ProductsRestClientImpl productsRestClient(@Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
+        return new ProductsRestClientImpl(builder().baseUrl(catalogueBaseUri).build());
     }
 }
